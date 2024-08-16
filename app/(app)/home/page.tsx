1aa94ed10209c309fb0bb58738a10c724c840296
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-16 12:37:11 +0600

    fetch videos

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+import React,{useState,useEffect,useCallback} from 'react'
+import axios from 'axios'
 import VideoCard from '@/components/VideoCard'
-import React from 'react'
+import { Video } from '@/types'
+
 
 const Home = () => {
+  const [videos,setVideos] =useState<Video[]>([])
+  const [loading,setLoading] =useState(true)
+  const [error,setError] =useState<string | null>(null)
+
+  const fetchVideos = useCallback(async() =>{
+    try {
+      const response =await axios.get("/api/videos")
+      if(Array.isArray(response.data)){
+        setVideos(response.data)
+      } else{
+        throw new Error("unexpected response format")
+      }
+    } catch (error) {
+      console.log(error)
+      setError("Failed To fetch videos")
+    } finally {
+      setLoading(false)
+    }
+  },[])
+
+
+  useEffect(()=> {
+      fetchVideos()
+  },[fetchVideos])
+
+
   return (
     <VideoCard/>
   )
